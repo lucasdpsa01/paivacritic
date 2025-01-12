3e406297592b55bc6c9ed862f5a7f1574a199f62
lucasdpsa01 <lucas.psa@sempreceub.com>
2025-01-11 22:34:54 -0300

    sla

--- melao/front/src/Components/Mains/MainHome/index.jsx
+++ melao/front/src/Components/Mains/MainHome/index.jsx
@@ -5,10 +5,10 @@ import "./mainhome.css"
 export default function MainHome() {
     return (
         <div className="main-home">
-            <div className="">
+            <div className="main-h1">
                 <h1>REVIEW DOS JOGOS QUE JOGUEI EM 2024</h1>
             </div>
-            <div>
+            <div className="main-cardmain">
                 <CardMain/>
             </div>
         </div>
