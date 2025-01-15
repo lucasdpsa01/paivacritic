8119761e802cbe57bdcacdf818abeef177ba3afb
lucasdpsa01 <lucas.psa@sempreceub.com>
2025-01-15 13:23:10 -0300

    sugestoes indisponivel pronto!!!

--- melao/front/src/Components/Background/BackgroundSugestao/index.jsx
+++ melao/front/src/Components/Background/BackgroundSugestao/index.jsx
@@ -1,8 +1,10 @@
 import imageb from "../../../images/emoji.png"
 
+import "./index.css"
+
 export default function BackgroundSugestao() {
     return(
-        <div>
+        <div className="background-sugestao">
             <img src={imageb} />
             <h1>OPS! ESTA PAGINA ESTA INDISPONIVEL</h1>
         </div>
