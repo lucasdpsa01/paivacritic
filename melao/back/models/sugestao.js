8e73f1f515b69ecc2a4439106da1c00f7d6b6b77
lucasdpsa01 <lucas.psa@sempreceub.com>
2025-02-03 06:55:28 -0300

    resolvido mais ou menos

--- melao/back/models/sugestao.js
+++ melao/back/models/sugestao.js
@@ -1,24 +1,14 @@
-<<<<<<< HEAD
 const sequelize = require('../config/db');
 const datatype = require('sequelize');
 
 const Sugestao = sequelize.define('Sugestao', {
     id: {
         type:datatype.INTEGER,
-=======
-const Sequelize = require('sequelize');
-const database = require('../config/db');
-
-const Sugestao = database.define('sugestao', {
-    id: {
-        type:Sequelize.INTEGER,
->>>>>>> main
         autoIncrement:true,
         allowNull:false,
         primaryKey:true
     },
     nome: {
-<<<<<<< HEAD
         type: datatype.STRING,
         allowNull: false
     },
@@ -33,12 +23,3 @@ const Sugestao = database.define('sugestao', {
 module.exports = Sugestao;
 
 //COMPLETO 23/01
-=======
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    sugestao: Sequelize.STRING
-})
-
-module.exports = Sugestao
->>>>>>> main
