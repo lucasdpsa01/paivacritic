b5e301e84b56a3ff93ef742bce50dd7021ee499a
lucasdpsa01 <lucasdsa2801@gmail.com>
2025-08-10 00:05:39 -0300

    removi varias coisas (routes sugestion e index.js)

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,3 @@
-/*const express = require('express');
-const app = express();
-const cors = require ('cors');
-const PORT = 3000;
-
-
-const sequelize = require('./config/db');
-
-
-sequelize.sync({ alter: true })
-.then(() => {
-    console.log("BD sincronizado");
-})
-.catch(error => {
-    console.log("Erro!");
-});
-
-app.use(cors());
-app.use(express.json());
-
-
-app.listen(PORT, () => {
-    console.log("Servidor aguardando requisições");
-});
-*/
-
 const express = require('express');
 const cors = require('cors');
 const sequelize = require('./config/db');
