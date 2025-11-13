8f39c093c52a0deb05dc48ca67ee37cdaa608aee
lucasdpsa01 <lucasdsa2801@gmail.com>
2025-11-13 14:25:06 -0300

    .

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,18 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-    host: process.env.DB_HOST,
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
-    port: process.env.DB_PORT || 5432,
-    logging: false 
+    protocol: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    },
+    logging: false
 });
+
 
 // Teste de conexão
 sequelize.authenticate()
