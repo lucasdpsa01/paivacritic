2e09e5ad922bc3685699f477267d4e7246847b0d
lucasdpsa01 <lucasdsa2801@gmail.com>
2025-11-16 06:25:30 -0300

    espero q de certo

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,14 @@ require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
-    logging: false
+    protocol: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    },
+    logging: false,
 });
 
 
